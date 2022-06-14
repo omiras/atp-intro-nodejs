0fb275acc8a51ebb3a641142d8013a285f6c98c6
omiras <oscar.miras@gmail.com>
2022-06-14 17:48:49 +0200

    updated

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const data = fs.readFileSync('atp.json', 'utf8');
 // JSON.parse va a convertir el string JSON en un array de objetos que podamos manejar
 const tournaments = JSON.parse(data);
 
-
-
 // MANDAMIENTO 1: Para los ejercicios de servdiro NodeJS NO USAMOS Live Server. NodeJS va a ser nuestro LiveServer.
 
 // Importar el módulo interno http. El módulo http nos va a permitir crear servidores
@@ -75,7 +73,70 @@ let servidor = http.createServer((req, res) => {
 
     // Cómo obtengo el id del jugador por el cual queremos buscar los partidas ganador a partir de la varible req.url
     let id = req.url.split("player=")[1];
-    return res.end("Quiero los partidos ganados por el jugador " + id);
+
+    // Mostrar por consola:  recuperar un array con todos los partidos que ha ganado el jugador identificado con valor de la variable id.
+
+    // tournaments. Recuperar todos los objetos tales que la propiedad winner_id == id
+
+    // Opción 1: Recorrer todo el array torunaments y quedarte solo con los objetos talesque winner_id == id
+
+    // Opción 2: Utilizar adecuadamente el método filter para quedarte solo con los objetos que cumplen que winner_id == id
+
+    // Quiero filtrar todos los objetos que cumplen con la condición que el campo winner_id es exactamente el mismo que hemos pasado por parámetro en la QueryString. Además, uso .map para quedarme solo con el campo "torneu_name";
+
+    const tournamentsWon = tournaments.filter(t => t.winner_id == id);
+
+    res.write(`<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>`);
+
+    res.write(`<h1>Torneos ganados por el jugador con id ${id} </h1>`)
+
+    // TODO: forEach: crear un string enorme de varias <p>
+    res.write("<table>");
+    res.write("<tr>")
+    res.write("<th>");
+    res.write("Nombre Torneo")
+    res.write("</th>");
+    res.write("<th>");
+    res.write("Jugador derrotado")
+    res.write("</th>");
+    res.write("</tr>")
+
+
+    // generar tantas filas como elementos hay en el array
+    tournamentsWon.forEach(t => {
+      res.write("<tr>");
+      res.write("<td>");
+      res.write(t.tourney_name);
+      res.write("</td>");
+      res.write("<td>");
+      res.write(t.loser_name);
+      res.write("</td>");
+      res.write("</tr>");
+
+    })
+
+    res.write("</table>");
+
+
+    return res.end();
 
   }
 
